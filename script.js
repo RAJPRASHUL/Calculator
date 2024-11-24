88f1e1e6ea001499565d4621d484e3e5aa112eca
RAJPRASHUL <prashulraj522@gmail.com>
2024-11-24 11:30:13 +0530

    adding a file

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById('calculate').addEventListener('click', function() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const operation = document.getElementById('operation').value;
+    let result;
+  
+    if (isNaN(num1) || isNaN(num2)) {
+      result = 'Please enter valid numbers.';
+    } else {
+      switch (operation) {
+        case 'add':
+          result = num1 + num2;
+          break;
+        case 'subtract':
+          result = num1 - num2;
+          break;
+        case 'multiply':
+          result = num1 * num2;
+          break;
+        case 'divide':
+          result = num2 !== 0 ? num1 / num2 : 'Cannot divide by zero';
+          break;
+        default:
+          result = 'Invalid operation selected.';
+      }
+    }
+  
+    document.getElementById('result').textContent = `Result: ${result}`;
+  });
+  
